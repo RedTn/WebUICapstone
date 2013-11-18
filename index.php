1028303d3eb20708af8cb611645a002b5755ac82
Red <kobebry48@gmail.com>
2013-11-18 11:04:48 -0800

    remove comment test

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
  * To change the template for this generated file go to
  * Window - Preferences - PHPeclipse - PHP - Code Templates
  */
- //ivan comment
 ?>
 
 <form method = "LINK" Action="db/toothparam.php">
